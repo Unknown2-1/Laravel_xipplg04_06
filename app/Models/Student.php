542804d3324710a09f5b1e9e5efd0fd425ace1d6
Unknown2-1 <dnshadelio@gmail.com>
2025-11-03 15:20:05 +0700

    Add Migration Student

---
+++ app/Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Student extends Model
+{
+    use HasFactory;
+
+    // Tambahkan properti $fillable ini
+    protected $fillable = [
+        'nis',
+        'nama_lengkap',
+        'jenis_kelamin',
+        'nisn',
+    ];
+}
